990a54c252314cb604d9062317702dd3a6547c9f
Brian Anders <anders.brian@icloud.com>
2016-04-20 22:23:14 -0700

    Keep uncompressed bersions of the build.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     storeConfigInMeta: false,
+    gzip: {
+      keepUncompressed: true
+    },
     minifyJS: {
       enabled: EmberApp.env() === 'production'
     },
